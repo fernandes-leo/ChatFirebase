ebe156ac75af9e47160b5df40e20769778c23789
Leonardo <fernandes.leo@live.com>
2022-04-07 09:09:25 -0300

    Initial Commit

---
+++ ChatFirebase/ChatFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatFirebaseApp.swift
+//  ChatFirebase
+//
+//  Created by Leonardo Fernandes on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
